f530b9d58e8028e6869a22529ec4095186d2245a
Matthew Barnard <msbarnar@gmail.com>
2013-06-08 16:02:35 -0700

    User interface improvement; implementing Observer pattern

--- ying-mapreduce/src/main/java/edu/asu/ying/mapreduce/webui/NodeServlet.java
+++ ying-mapreduce/src/main/java/edu/asu/ying/mapreduce/webui/NodeServlet.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 /**
  * Exposes information about the node found at the key specified in the url 
  */
@@ -13,15 +15,36 @@ public final class NodeServlet
 {
 	private static final long serialVersionUID = -545603243684031979L;
 	
+	/**
+	 * Default response is the status of the node:
+	 * <p>
+	 * <pre>
+	 * {@code
+	 * <node>
+	 * 	<status>error</status>
+	 * 	<message>RuntimeException</message>
+	 * </node>
+	 * }
+	 * </pre>
+	 */
 	public void doGet(final HttpServletRequest request, final HttpServletResponse response)
 			throws ServletException, IOException {
-		final String resp = "<status>Running</status>";
-		
+		//final String resp = "<node><status>error</status><message>RuntimeException: Don't know what I'm doing!</message></node>";
+		//final String resp = "<node><status>down</status></node>";
+		final String resp = this.getNodeStatus();
 		response.setContentType("text/xml");
 		response.setHeader("Cache-Control", "no-cache");
 		response.setContentLength(resp.length());
 		response.setStatus(HttpServletResponse.SC_OK);
 		response.getWriter().println(resp);
 		response.flushBuffer();
+	}
+	
+	private final String getNodeStatus() {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("<node><status>");
+		sb.append("running");
+		sb.append("</status></node>");
+		return sb.toString();
 	}
 }
--- ying-mapreduce/www/js/index.js
+++ ying-mapreduce/www/js/index.js
@@ -4,9 +4,8 @@ $(document).ready(initialize());
 
 function initialize() {
 	// Update node info every 3 seconds
-	setInterval(function() {
-		nodeInfoReq = makeRequest("node", updateConnectionStatus);
-	}, 3000);
+	setInterval(checkConnectivity, 3000);
+	checkConnectivity();
 }
 
 function getXmlHttpRequest() {
@@ -17,35 +16,62 @@ function getXmlHttpRequest() {
 	}
 }
 
-function makeRequest(url, callback) {
-	var req = getXmlHttpRequest();
-	req.open("GET", "http://localhost:8777/" + url, true);
-	req.onreadystatechange = callback;
-	req.send(null);
-	return req;
+function makeRequest(url, doneCallback, failCallback) {
+	return $.ajax("http://localhost:8777/" + url).done(doneCallback).fail(failCallback);
 }
 
 // Show in status bar if we're connected to the local node
-function checkConnectivity(req) {
-	if (req.readyState == 4 && req.status == 200) {
-		$("span#connectionStatus").text("connected");
-		$("img#connection-off").addClass("nodisplay");
-		$("img#connection-on").removeClass("nodisplay");
-		$("div.section-not-connected").addClass("nodisplay");
-		$("div.section-body").removeClass("nodisplay");
-		return true;
+function checkConnectivity() {
+	makeRequest("node", displayConnected, displayDisconnected);
+}
+
+function displayConnected(data) {
+	$("span#connectionStatus").text("connected");
+	$("img#connection-off").addClass("nodisplay");
+	$("img#connection-on").removeClass("nodisplay");
+	$("div.section-not-connected").addClass("nodisplay");
+	$("div.section-body").removeClass("nodisplay");
+	getNodeStatus(data);
+}
+function displayDisconnected() {
+	$("span#connectionStatus").text("not connected");
+	$("img#connection-off").removeClass("nodisplay");
+	$("img#connection-on").addClass("nodisplay");
+	$("div.section-not-connected").removeClass("nodisplay");
+	$("div.section-body").addClass("nodisplay");
+}
+
+function getNodeStatus(data) {
+	var nodeStatus = data.getElementsByTagName("status")[0].childNodes[0].nodeValue;
+	if (nodeStatus.toLowerCase() == "running") {
+		displayNodeRunning();
+	} else if (nodeStatus.toLowerCase() == "down") {
+		displayNodeDown();
+	} else if (nodeStatus.toLowerCase() == "error") {
+		displayNodeError(data.getElementsByTagName("message")[0].childNodes[0].nodeValue);
 	} else {
-		$("span#connectionStatus").text("not connected");
-		$("img#connection-off").removeClass("nodisplay");
-		$("img#connection-on").addClass("nodisplay");
-		$("div.section-not-connected").removeClass("nodisplay");
-		$("div.section-body").addClass("nodisplay");
-		return false;
+		displayNodeError("Unknown status: " + nodeStatus);
 	}
 }
 
-function updateConnectionStatus() {
-	checkConnectivity(nodeInfoReq);
+function displayNodeRunning() {
+	$("li#node-running").removeClass("nodisplay");
+	$("li#node-down").addClass("nodisplay");
+	$("li#node-error").addClass("nodisplay");
+	$("li#node-error-message").addClass("nodisplay");
+}
+function displayNodeDown() {
+	$("li#node-running").addClass("nodisplay");
+	$("li#node-down").removeClass("nodisplay");
+	$("li#node-error").addClass("nodisplay");
+	$("li#node-error-message").addClass("nodisplay");
+}
+function displayNodeError(errMessage) {
+	$("li#node-running").addClass("nodisplay");
+	$("li#node-down").addClass("nodisplay");
+	$("li#node-error").removeClass("nodisplay");
+	$("li#node-error-message").removeClass("nodisplay");
+	$("li#node-error-message").text(errMessage);
 }
 
 function switch_tab_network() {
