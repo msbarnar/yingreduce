f0ae1bb30f50af11e7fbacedfbee582ab1bab0b8
Matthew Barnard <msbarnar@gmail.com>
2013-09-04 11:15:56 -0700

    Cleaning up job/task definitions

--- mapreduce/src/main/java/edu/asu/ying/mapreduce/mapreduce/job/Job.java
+++ mapreduce/src/main/java/edu/asu/ying/mapreduce/mapreduce/job/Job.java
@@ -1,9 +1,12 @@
 package edu.asu.ying.mapreduce.mapreduce.job;
 
 import java.io.Serializable;
+import java.net.InetAddress;
 
 import edu.asu.ying.mapreduce.mapreduce.task.TaskID;
 import edu.asu.ying.mapreduce.yingtable.TableID;
+import edu.asu.ying.p2p.NodeIdentifier;
+import edu.asu.ying.p2p.RemoteNode;
 
 /**
  * {@code Job} is the base interface for a full map/reduce job.
@@ -12,4 +15,6 @@ public interface Job extends Serializable {
 
   TaskID getId();
   TableID getSourceTableId();
+
+  RemoteNode getResponsibleNode();
 }
--- mapreduce/src/main/java/edu/asu/ying/mapreduce/mapreduce/task/Task.java
+++ mapreduce/src/main/java/edu/asu/ying/mapreduce/mapreduce/task/Task.java
@@ -4,7 +4,9 @@ import java.io.Serializable;
 import java.net.InetAddress;
 
 import edu.asu.ying.mapreduce.common.Properties;
+import edu.asu.ying.mapreduce.mapreduce.job.Job;
 import edu.asu.ying.p2p.NodeIdentifier;
+import edu.asu.ying.p2p.RemoteNode;
 
 /**
  * A {@code Task} is the basic unit of work in the map/reduce system.
@@ -15,30 +17,31 @@ public interface Task extends Serializable {
 
   Properties getProperties();
 
-  Serializable run();
+  Job getParentJob();
 
   TaskID getId();
+
+  /**
+   * Returns a {@link java.rmi.Remote} proxy to the node which owns the data to which this task
+   * applies.
+   */
+  RemoteNode getInitialNode();
+
+  /**
+   * Returns the parameters used to manage the starting of this task on its final executing node.
+   */
   TaskStartParameters getTaskStartParameters();
 
   /**
-   * Returns a history of all of the schedulers that have observed this mapreduce.
+   * Returns a history of all of the schedulers that have handled this task.
    */
   TaskHistory getHistory();
 
-  /**
-   * Returns the IP address of the node responsible for managing the mapreduce (the owner of the first
-   * mapreduce segment).
-   */
-  InetAddress getResponsibleNodeAddress();
-
-  void touch(final NodeIdentifier uri);
+  Serializable run();
 
   /**
-   * Returns {@code true} if this is the initial node (the node carrying the table) for the specified
-   * mapreduce.
-   * </p>
-   * This is the initial node if the most recent node to handle the mapreduce was the
-   * {@code responsible node} for the job.
+   * Returns {@code true} if this is the initial node (the node carrying the table) for the
+   * specified mapreduce.
    */
   boolean isCurrentlyAtInitialNode();
 }
