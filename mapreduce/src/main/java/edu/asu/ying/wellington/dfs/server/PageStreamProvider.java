85b17467d43ea39d2d0a0477cfa199805bbef76f
Matthew Barnard <msbarnar@gmail.com>
2013-10-16 19:16:22 -0700

    Writable serialization

--- mapreduce/src/main/java/edu/asu/ying/wellington/dfs/server/PageStreamProvider.java
+++ mapreduce/src/main/java/edu/asu/ying/wellington/dfs/server/PageStreamProvider.java
@@ -2,8 +2,10 @@ package edu.asu.ying.wellington.dfs.server;
 
 /**
  * {@code PageStreamProvider} provides streams for reading and writing page data from and to
- * storage. </p> If the page is stored on another node, the provider will retrieve it and cache it
- * locally. The provided stream will then be serviced by that cache.
+ * storage.
+ * </p>
+ * If the page is stored on another node, the provider will retrieve it and cache it locally.
+ * The provided stream will then be serviced by that cache.
  */
 public final class PageStreamProvider {
 
