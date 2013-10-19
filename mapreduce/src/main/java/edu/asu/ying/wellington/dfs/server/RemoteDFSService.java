8ac7fdb22c924a3a0c6b1c2067450e3305b94dc4
Matthew Barnard <msbarnar@gmail.com>
2013-10-18 22:54:36 -0700

    Brought in RMIIO

--- mapreduce/src/main/java/edu/asu/ying/wellington/dfs/server/RemoteDFSService.java
+++ mapreduce/src/main/java/edu/asu/ying/wellington/dfs/server/RemoteDFSService.java
@@ -1,16 +1,11 @@
 package edu.asu.ying.wellington.dfs.server;
 
-import java.rmi.RemoteException;
-
-import edu.asu.ying.common.event.RemoteSink;
 import edu.asu.ying.common.remoting.Activatable;
-import edu.asu.ying.wellington.dfs.PageMetadata;
 
 /**
  * {@code RemoteDFSService} is the interface to the database server on a remote peer.
  */
 public interface RemoteDFSService extends Activatable {
 
-
-  RemoteSink<PageMetadata> getPageDepository() throws RemoteException;
+  
 }
