099e6930403b8bc236279dfff7ebdafa21b1520d
Matthew Barnard <msbarnar@gmail.com>
2013-10-26 00:24:53 -0700

    Revamping persistence for more unified control

--- mapreduce/src/main/java/edu/asu/ying/wellington/dfs/persistence/Persistence.java
+++ mapreduce/src/main/java/edu/asu/ying/wellington/dfs/persistence/Persistence.java
@@ -1,18 +1,9 @@
 package edu.asu.ying.wellington.dfs.persistence;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import edu.asu.ying.wellington.dfs.PageIdentifier;
-
 /**
  * A {@code Persistence} provides uniform access to an underlying persistence engine
  * be it a file, relational database, or memory cache.
  */
 public interface Persistence {
 
-  OutputStream getOutputStream(PageIdentifier id) throws IOException;
-
-  InputStream getInputStream(PageIdentifier id) throws IOException;
 }
