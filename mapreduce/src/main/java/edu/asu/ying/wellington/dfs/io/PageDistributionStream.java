c4e31d6950d9837f899ac834f74d9208ab7d13c3
Matthew Barnard <msbarnar@gmail.com>
2013-10-26 17:36:16 -0700

    More appropriate name

--- mapreduce/src/main/java/edu/asu/ying/wellington/dfs/io/PageDistributionStream.java
+++ mapreduce/src/main/java/edu/asu/ying/wellington/dfs/io/PageDistributionStream.java
@@ -11,12 +11,12 @@ import edu.asu.ying.wellington.dfs.Page;
 import edu.asu.ying.wellington.dfs.PageData;
 
 /**
- * {@code BufferedPageOutputStream} buffers output up to a single page's capacity, then sends that
+ * {@code PageDistributionStream} buffers output up to a single page's capacity, then sends that
  * page to its responsible node.
  */
-public final class BufferedPageOutputStream extends OutputStream {
+public final class PageDistributionStream extends OutputStream {
 
-  private final Logger log = Logger.getLogger(BufferedPageOutputStream.class.getName());
+  private final Logger log = Logger.getLogger(PageDistributionStream.class.getName());
 
   private final Sink<PageData> pageDistributor;
 
@@ -25,7 +25,7 @@ public final class BufferedPageOutputStream extends OutputStream {
 
   private Page currentPage;
 
-  public BufferedPageOutputStream(Page firstPage, Sink<PageData> pageDistributor) {
+  public PageDistributionStream(Page firstPage, Sink<PageData> pageDistributor) {
 
     this.pageDistributor = pageDistributor;
     this.currentPage = firstPage;
